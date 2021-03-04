e572ce8895f16ba82a82ae18605340105865b74c
MeyerFrenchel <meyer.frenchel@gmail.com>
2021-03-04 15:47:31 +0200

    navbar colapse

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ function changeCircleColor(color) {
     const circle = document.querySelector('.circle');
     circle.style.background = color; 
 }
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation')
+let toggleMenu = () => {
+    
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+    menuToggle.addEventListener('click', toggleMenu);
+
+
+
